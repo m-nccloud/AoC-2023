7ab144733fbd12fc4ce807e0f3e98cafca959200
Savva Madar <37639781+savvamadar@users.noreply.github.com>
2023-12-06 22:53:46 -0500

    Create day1p1.py

---
+++ day1p1.py
@@ -0,0 +1,17 @@
+import re
+
+def remove_non_num(inp):
+    return re.sub(r'[^0-9]', '', inp)
+
+example_input = '''1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet'''
+
+total = 0
+
+for line in example_input.split():
+    nums = remove_non_num(line)
+    total += int(nums[0])*10 + int(nums[0]) if len(nums) == 1 else int(nums[0])*10 + int(nums[len(nums)-1]) #doesnt account for no numbers in nums ik
+    
+print(total)
