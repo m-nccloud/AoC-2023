231790eba2b6886e55067edef2049bfc7d0d79a2
Savva Madar <37639781+savvamadar@users.noreply.github.com>
2023-12-07 01:10:07 -0500

    Create day6p1.py

---
+++ day6p1.py
@@ -0,0 +1,15 @@
+races = [[7,9],[15,40],[30,200]]
+
+possible_wins = []
+for race in races:
+    possible_wins.append([])
+    for time in range(race[0]):
+        _t = time * (race[0] - time)
+        if _t > race[1]:
+            possible_wins[len(possible_wins)-1].append(_t)
+            
+total = 1#assumes at least one possiblity in all races can win
+for x in [len(i) for i in possible_wins]:
+    total *= x
+    
+print(total)
