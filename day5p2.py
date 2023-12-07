78a0c248cd3bda91ceab18b931632dd8128606c5
Savva Madar <37639781+savvamadar@users.noreply.github.com>
2023-12-06 22:38:46 -0500

    Update day5p2.py

--- day5p2.py
+++ day5p2.py
@@ -1,3 +1,6 @@
+def mapper(mapstring):
+    return [[int(x) for x in line.split()] for line in mapstring.split('\n')]
+
 def remapper(rules, inp):
     for rule in rules:
         if rule[1] <= inp and inp < rule[1]+rule[2]:
@@ -10,41 +13,41 @@ seeds_sets = [[79,14],[55,13]]
 seed_to_soilmap = '''50 98 2
 52 50 48'''
 
-seed_to_soil_rules = [[int(x) for x in line.split()] for line in seed_to_soilmap.split('\n')]
+seed_to_soil_rules = mapper(seed_to_soilmap)
 
 soil_to_fertilizermap='''0 15 37
 37 52 2
 39 0 15'''
 
-soil_to_fertilizer_rules = [[int(x) for x in line.split()] for line in soil_to_fertilizermap.split('\n')]
+soil_to_fertilizer_rules = mapper(soil_to_fertilizermap)
 
 fertilizer_to_watermap='''49 53 8
 0 11 42
 42 0 7
 57 7 4'''
 
-fertilizer_to_water_rules = [[int(x) for x in line.split()] for line in fertilizer_to_watermap.split('\n')]
+fertilizer_to_water_rules = mapper(fertilizer_to_watermap)
 
 water_to_lightmap='''88 18 7
 18 25 70'''
 
-water_to_light_rules = [[int(x) for x in line.split()] for line in water_to_lightmap.split('\n')]
+water_to_light_rules = mapper(water_to_lightmap)
 
 light_to_temperaturemap='''45 77 23
 81 45 19
 68 64 13'''
 
-light_to_temperature_rules = [[int(x) for x in line.split()] for line in light_to_temperaturemap.split('\n')]
+light_to_temperature_rules = mapper(light_to_temperaturemap)
 
 temperature_to_humiditymap='''0 69 1
 1 0 69''' #nice
 
-temperature_to_humidity_rules = [[int(x) for x in line.split()] for line in temperature_to_humiditymap.split('\n')]
+temperature_to_humidity_rules = mapper(temperature_to_humiditymap)
 
 humidity_to_locationmap='''60 56 37
 56 93 4'''
 
-humidity_to_location_rules = [[int(x) for x in line.split()] for line in humidity_to_locationmap.split('\n')]
+humidity_to_location_rules = mapper(humidity_to_locationmap)
 
 lln=-1
 
